bddbb6f133c5ea86758a69dc68d8f441fa0a42a6
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-20 15:05:07 -0500

    Completed Lesson 275

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+
+    void Start()
+    {
+        foreach (Block waypoint in path)
+        {
+            print(waypoint.name);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}

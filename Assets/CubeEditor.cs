2a23eec90cb7f36194e840603068da6ff1d8dd3c
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-19 19:06:18 -0500

    Completed Lesson 274

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField][Range(1f, 20f)] float gridSize = 10f;
+    TextMesh textMesh;
 
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * 10f;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * 10f;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }

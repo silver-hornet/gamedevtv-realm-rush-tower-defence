383e577b24092090b7684327935348291e4e2be6
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-21 19:51:16 -0500

    Completed Lesson 280

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.Log("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+}

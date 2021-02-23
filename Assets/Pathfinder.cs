d218542b772fcbc587d39dfe9c39adb46977a7ca
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-23 08:25:57 -0500

    Completed Lesson 336

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -8,13 +8,39 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Waypoint startWaypoint;
     [SerializeField] Waypoint endWaypoint;
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    [SerializeField] bool isRunning = true; // todo make private
     Vector2Int[] directions = { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
 
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
-        ExploreNeighbours();
+        Pathfind();
+        //ExploreNeighbours();
+    }
+
+    void Pathfind()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while(queue.Count > 0)
+        {
+            var searchCenter = queue.Dequeue();
+            print("Searching from " + searchCenter); // todo remove log
+            HaltIfEndFound(searchCenter);
+        }
+
+        print("Finished pathfinding?");
+    }
+
+    void HaltIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Searching from end node, therefore stopping"); // todo remove log
+            isRunning = false; // stops the Breadth First Search algorithm
+        }
     }
 
     void ExploreNeighbours()

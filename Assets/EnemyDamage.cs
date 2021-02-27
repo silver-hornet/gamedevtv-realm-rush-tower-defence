f2d2c289ee254848abb63fa3502ffcd2a1cb7a91
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-27 10:43:10 -0500

    Completed Lesson 144

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        print("I'm hit");
+    }
+}

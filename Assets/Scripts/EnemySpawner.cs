d377b11f230418255647c4e4f51411d48051c76e
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-28 15:58:12 -0500

    Completed Lesson 348

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemiesPrefab; // this ensures that we can only drag in a prefab that has the EnemyMovement script on it, rather than just dragging in any game object accidentally
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true) // forever
+        {
+            Instantiate(enemiesPrefab);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}

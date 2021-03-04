9a6a06e047de7d61a157df6ae3ecf904e71363fe
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-04 08:37:26 -0500

    Completed Lesson 361

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -8,9 +9,13 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] EnemyMovement enemiesPrefab; // this ensures that we can only drag in a prefab that has the EnemyMovement script on it, rather than just dragging in any game object accidentally
     [SerializeField] Transform enemyParentTransform;
 
+    int score;
+    [SerializeField] Text spawnedEnemies;
+
     // Start is called before the first frame update
     void Start()
     {
+        spawnedEnemies.text = score.ToString();
         StartCoroutine(RepeatedlySpawnEnemies());
     }
 
@@ -18,9 +23,16 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true) // forever
         {
+            AddScore();
             var newEnemy = Instantiate(enemiesPrefab, transform.position, Quaternion.identity);
             newEnemy.transform.parent = enemyParentTransform;
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    void AddScore()
+    {
+        score++;
+        spawnedEnemies.text = score.ToString();
     }
 }
